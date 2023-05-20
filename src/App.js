2719fa9d54f094a65c713d7b8aaf711e0ec9b343
Shahidul Islam <shahid.okay@gmail.com>
2023-05-20 23:54:49 +0100

    first and final deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [counter, setCounter] = useState(0)
+    useEffect(() => {
+        setCounter(100)
+    }, [])
+
+    return (
+        <div className="App">
+            <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+                -
+            </button>
+            <h1>{counter}</h1>
+            <button onClick={() => setCounter((nextCount) => nextCount + 1)}>
+                +
+            </button>
+        </div>
+    )
 }
 
-export default App;
+export default App
