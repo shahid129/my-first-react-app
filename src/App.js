ba5966fe45cb8652c3cf3d058e3909c3ab5edacf
Shahidul Islam <shahid.okay@gmail.com>
2023-05-20 23:55:53 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import './App.css'
-import { useState, useEffect } from 'react'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+// 6aac5d3a api key
+
+const API_URL = "https://www.omdbapi.com?apikey=6aac5d3a";
+
+const movie1 = {
+  Title: "Italian Spiderman",
+  Year: "2007",
+  imdbID: "tt2705436",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg",
+};
 
 const App = () => {
-    const [counter, setCounter] = useState(0)
-    useEffect(() => {
-        setCounter(100)
-    }, [])
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    return (
-        <div className="App">
-            <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
-                -
-            </button>
-            <h1>{counter}</h1>
-            <button onClick={() => setCounter((nextCount) => nextCount + 1)}>
-                +
-            </button>
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src="{SearchIcon}"
+          alt="Search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => {
+            return <MovieCard movie={movie} />;
+          })}
         </div>
-    )
-}
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
